a9dfba35ed0782e4edeed708c159025260947f83
Antoine GUERIN <antoineg3802@gmail.com>
2025-05-06 16:29:42 +0200

    feat: ts-ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// @ts-ignore
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
@@ -8,6 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // @ts-ignore
       "@": path.resolve(__dirname, "./src"),
     },
   },
